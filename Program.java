c103bab725c844fbf17878710527a7e774493332
joaogomes1806 <joao.perestrelo18@gmail.com>
2026-09-24 19:53:07 -0300

    Update student and professor details in Program.java

--- Program.java
+++ Program.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package program;
 
-/**
- *
- * @author Admin
- */
+
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         
         
         
-        Aluno aluno = new Aluno("Giovanni ",20,"ma25687" );
+        Aluno aluno = new Aluno("Joao Gomes Neto ",18,"ma27233" );
         aluno.apresentaraluno();
         
         System.out.println("---------------------------------");
         
-        Professor professor = new Professor ("Gilmar" ,40," PW1" );
+        Professor professor = new Professor ("Wagner" ,40," PTCC" );
         professor.apresentarprofessor();
         
         
